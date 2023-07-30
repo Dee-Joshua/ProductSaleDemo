0b43c1b73097f8f5e3707e2715983f381c8bc760
Dee-Joshua <officialdariajoshua@gmail.com>
2023-07-31 00:10:04 +0100

    New changes added

---
+++ ProductSale.Repository/Repository/Abstraction/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductSaleModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSaleRepository.Repository.Abstraction
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllCustomers();
+
+        Task<Customer> GetCustomerById(int id);
+    }
+}
---
+++ ProductSale.Repository/Repository/Implementation/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductSaleModel.Model;
+using ProductSaleRepository.Data;
+using ProductSaleRepository.Repository.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSaleRepository.Repository.Implementation
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly DbSet<Product> _dbSet;
+        public ProductRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+            _dbSet = appDbContext.Set<Product>();
+        }
+
+    }
+}
